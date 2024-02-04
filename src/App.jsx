dce8721b2eabb772193eb1655b51ac6ed37c2ff5
Aniket Gupta <aniketgupta.org@gmail.com>
2024-02-05 02:15:13 +0530

    feat: loader added, setup api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,33 @@ import './App.css'
 import { FaSearch } from "react-icons/fa";
 import Card from './components/Card';
 import Header from './components/Header';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
+  const API_URL = "https://restcountries.com/v3.1";
 
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const response = await axios.get("https://restcountries.com/v3.1/all");
-          console.log("Success", response.data);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
+  const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    async function fetchData(name, region) {
+      setLoader(true);
+      try {
+        const response = await axios.get(`${API_URL}${name ? `/name/${name}` : region ? `/region/${region}` : `/all`}`);
+
+        setData(response.data);
+
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-  
-      fetchData();
-    }, []);
+      setLoader(false);
+    }
+
+    fetchData();
+
+  }, []);
+
 
   return (
     <div className='relative *:box-border'>
@@ -29,21 +39,48 @@ function App() {
         <section className='flex justify-evenly w-full flex-wrap mx-10 items-center'>
 
           <div className='w-[300px] h-11 m-5 shadow-lg rounded-md flex items-center'>
-          <FaSearch className='w-[20px] mx-2'/>
-          <input type="search" className='focus:outline-none px-3 w-[100%] h-[100%]' placeholder='Search for any country...'/>
+            <FaSearch className='w-[20px] mx-2' />
+            <input type="search" className='focus:outline-none px-3 w-[100%] h-[100%]' placeholder='Search for any country...' />
           </div>
 
           <div className='w-[250px] h-11 m-5 shadow-lg rounded-md overflow-hidden'>
             <select name="region" className='w-[100%] h-[100%] px-3 bg-inherit focus:outline-none cursor-pointer'>
               <option hidden>Filter By Region</option>
-              <option >India</option>
-              <option >India</option>
-              <option >India</option>
+              <option value="all" default>All</option>
+
+              {
+              
+              data.map(country => country.region)
+              .filter((region, index, self) => self.indexOf(region) === index)
+                .map((region, index) => (
+                  <option key={index} value={region} >
+                    {region}
+                  </option>
+                ))
+              }
+
             </select>
           </div>
 
         </section>
-        <Card></Card>
+
+        {
+          loader?(<div className='mt-10 text-md'>
+                      Loading...
+          </div>):
+          data.map((country, index) => (
+            <Card
+              key={index}
+              name={country.name.common}
+              pop={country.population}
+              capital={country.capital}
+              flag={country.flags.svg}
+              region={country.region}
+            />
+          ))
+        }
+
+
       </main>
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,17 @@
 
-const Card = () => {
+const Card = (prop) => {
     return (
+
         <div className="rounded-lg m-7 border-2 shadow-lg overflow-hidden max-h-[400px] w-[300px] hover:-translate-y-2 hover:transition ease-in cursor-pointer">
             <div className="w-[100%] h-[180px] bg-cover">
-                <img className="h-[100%] w-[100%]" src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png" alt="inde" />
+                <img className="h-[100%] w-[100%]" src={prop.flag} alt="inde" />
                 </div>
                 <div className="p-5">
-                    <h2 className=" font-bold text-2xl my-2">India</h2>
+                    <h2 className=" font-bold text-2xl my-2">{prop.name.length>=15?prop.name.slice(0,15)+".....":prop.name}</h2>
                     <hr />
-                    <p className="my-1"><b>region:</b> South Asia</p>
-                    <p className="my-1"><b>population:</b> 320000</p>
-                    <p className="my-1"><b>Capital:</b> New Delhi</p>
+                    <p className="my-1"><b>Region : </b>{prop.region}</p>
+                    <p className="my-1"><b>Population : </b>{prop.pop}</p>
+                    <p className="my-1"><b>Capital : </b> {prop.capital}</p>
                 </div>
           
         </div>
