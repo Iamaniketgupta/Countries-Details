415420f5fd69f0e8d6b31ca6c14c8371b74aeacb
Aniket Gupta <aniketgupta.org@gmail.com>
2024-02-06 23:39:10 +0530

    added image alt attribute values

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,26 @@
 import { MdReadMore } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 
 const Card = (prop) => {
     return (
 
         <div className="rounded-lg m-7 border-2 shadow-lg overflow-hidden max-h-[500px] w-[300px] hover:-translate-y-2 hover:transition ease-in cursor-pointer">
-            <div className="w-[100%] h-[180px] bg-cover">
-                <img className="h-[100%] w-[100%]" src={prop.flag} alt="inde" />
+
+           <NavLink to={"/countrydetail?name="+`${prop.name}`}> 
+           <div className="w-[100%] h-[180px] bg-cover">
+                <img className="h-[100%] w-[100%]" src={prop.flag} alt={prop.alt} />
                 </div>
-                <div className="p-5">
+                <div className="p-5 leading-8">
                     <h2 className=" font-bold text-2xl my-2">{prop.name.length>=15?prop.name.slice(0,15)+".....":prop.name}</h2>
                     <hr />
-                    <p className="my-1"><b>Region : </b>{prop.region}</p>
-                    <p className="my-1"><b>Population : </b>{prop.pop.toLocaleString("en-IN")}</p>
-                    <p className="my-1"><b>Capital : </b> {prop.capital}</p>
+                    <p><b>Region : </b>{prop.region}</p>
+                    <p><b>Population : </b>{prop.pop.toLocaleString("en-IN")}</p>
+                    <p><b>Capital : </b> {prop.capital}</p>
                 </div>
                 <div className="h-5 mx-3 mb-4 float-end px-3 text-2xl  hover:text-blue-600 ">
                 <MdReadMore title="Read more" />
                 </div>
+                </NavLink>
         </div>
     );
 }
