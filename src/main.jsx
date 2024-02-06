94facc9d7221611243d961a7652e477fd7aa031e
Aniket Gupta <aniketgupta.org@gmail.com>
2024-02-06 23:40:47 +0530

    added routes and paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import CardDetails from './components/CardDetails.jsx'
+import Error from './components/Error.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error/>
+  },
 
+  {
+    path: "/countrydetail",
+    element: <CardDetails />,
+    // errorElement: <Error/>
+
+  },
+
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
