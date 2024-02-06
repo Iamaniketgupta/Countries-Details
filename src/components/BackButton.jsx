ac71085306f50657e9f7622a6c409995d74ca9a6
Aniket Gupta <aniketgupta.org@gmail.com>
2024-02-06 23:37:48 +0530

    chore : added

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,13 @@
+import { FaArrowLeft } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+
+const BackButton = () => {
+    return (
+        <>
+    <NavLink to='/' className='flex items-center justify-center text-xl border-2 shadow-sm px-3 py-2 rounded-lg font-semibold text-gray-700 hover:text-black '><FaArrowLeft className='mr-2 text-blue-700 hover:text-blue-950' /> Back</NavLink>
+        </>
+    );
+}
+
+export default BackButton;
